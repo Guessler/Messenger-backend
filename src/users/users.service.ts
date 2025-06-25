2248d6b714bfd3292793b1f98692ba5b7bef18c2
unknown <tarasovlev7@gmail.com>
2025-06-25 15:38:36 +0300

    all-user-roles

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,18 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { CreateUserDto } from './dto/create-user.dto';
+import { RolesService } from 'src/roles/roles.service';
 
 @Injectable()
 export class UsersService {
-    constructor (@InjectModel(User) private userRepository: typeof User) {
+    constructor(@InjectModel(User) private userRepository: typeof User,
+        private roleService: RolesService) {
 
     }
-    async createUser (dto: CreateUserDto) {
+    async createUser(dto: CreateUserDto) {
         const user = await this.userRepository.create(dto);
+        const role = await this.roleService.getRoleByValue("USER");
+
+        if (!role) {
+            throw new Error('Default role "USER" not found');
+        }
+
+        await user.$set('roles', [role.id]);
         return user;
     }
-    async getAllUsers () {
-        const users = await this.userRepository.findAll();
+    async getAllUsers() {
+        const users = await this.userRepository.findAll({
+            include: ['roles']
+        });
         return users
     }
 }
