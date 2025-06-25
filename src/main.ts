40001f9448c845b76635db50ff4698a0fd4a40ea
unknown <tarasovlev7@gmail.com>
2025-06-25 23:47:34 +0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 5000;
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    method: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+
   const config = new DocumentBuilder()
-  .setTitle('Messenger')
-  .setDescription('fast messenger for daily using')
-  .setVersion('1.0.0')
-  .addTag('TL')
-  .build()
+    .setTitle('Messenger')
+    .setDescription('fast messenger for daily using')
+    .setVersion('1.0.0')
+    .addTag('TL')
+    .build()
 
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('/api/docs', app, document)
